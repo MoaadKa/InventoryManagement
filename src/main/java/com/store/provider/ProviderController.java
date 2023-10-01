d24cd044c5c0c3af706d8903fed868575b1104e5
MoaadKa <moaadkarkouri@gmail.com>
2023-10-01 13:46:16 +0100

    created provider controller which will handle the crud requests

---
+++ src/main/java/com/store/provider/ProviderController.java
@@ -0,0 +1,42 @@
+package com.store.provider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/provider")
+public class ProviderController {
+
+    private final ProviderService providerService;
+
+    @Autowired
+    public ProviderController(ProviderService providerService) {
+        this.providerService = providerService;
+    }
+
+    @GetMapping
+    public List<Provider> getProviders(){
+        return providerService.getProviders();
+    }
+
+    @PostMapping
+    public void addProvider(@RequestBody Provider provider){
+        providerService.addProvider(provider);
+    }
+
+    @DeleteMapping(path = "{providerId}")
+    public void deleteProvider(@PathVariable("providerId") Long providerId){
+        providerService.deleteProvider(providerId);
+    }
+
+    @PutMapping(path = "{providerId}")
+    public void updateProvider(
+            @PathVariable("providerId") long providerId,
+            @RequestParam(required = false) String name
+    ){
+        providerService.updateProvider(providerId,name);
+
+    }
+}
