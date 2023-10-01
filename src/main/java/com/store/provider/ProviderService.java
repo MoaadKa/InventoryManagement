1f89df920469ab36ddcd3a9775b65c876fd2c7d1
MoaadKa <moaadkarkouri@gmail.com>
2023-10-01 13:33:49 +0100

    created provider service class which will be responsible for the business logic

---
+++ src/main/java/com/store/provider/ProviderService.java
@@ -0,0 +1,41 @@
+package com.store.provider;
+
+import com.store.product.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProviderService {
+
+    private final ProviderRepository providerRepository;
+
+    @Autowired
+    public ProviderService(ProviderRepository providerRepository) {
+        this.providerRepository = providerRepository;
+    }
+
+    public List<Provider> getProviders(){
+        return providerRepository.findAll();
+    }
+
+    public void addProvider(Provider provider){
+        providerRepository.save(provider);
+    }
+
+    public void deleteProvider(Long providerId){
+        if (!providerRepository.existsById(providerId)){
+            throw new IllegalStateException("Provider with id: "+providerId+" doesn't exist");
+        }
+        providerRepository.deleteById(providerId);
+    }
+
+    public void updateProvider(Long providerId, String name){
+        Optional<Provider> optionalProvider = providerRepository.findById(providerId);
+        Provider provider = optionalProvider.orElseThrow(
+                ()-> new IllegalStateException("not found"));
+        if(name!=null && !name.isBlank()) provider.setName(name);
+    }
+}
