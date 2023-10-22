5e46cb26f4aee18dd892ad9031f9d47a86c0a5cb
MoaadKa <moaadkarkouri@gmail.com>
2023-10-22 20:16:16 +0100

    Created handler method for editing the stock

--- src/main/java/com/store/stock/StockThymeleafController.java
+++ src/main/java/com/store/stock/StockThymeleafController.java
@@ -2,9 +2,11 @@ package com.store.stock;
 
 import com.store.product.Product;
 import com.store.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +18,13 @@ public class StockThymeleafController {
 
     private final StockService stockService;
     private final ProductService productService;
+    private final StockRepository stockRepository;
 
-    public StockThymeleafController(StockService stockService, ProductService productService) {
+    @Autowired
+    public StockThymeleafController(StockService stockService, ProductService productService, StockRepository stockRepository) {
         this.stockService = stockService;
         this.productService = productService;
+        this.stockRepository = stockRepository;
     }
 
     @GetMapping("list")
@@ -39,5 +44,17 @@ public class StockThymeleafController {
     public String saveStock(Stock stock){
         stockService.addStock(stock);
         return "redirect:/api/v2/stock/list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editStock(
+            @PathVariable("id") Long id,
+            Model model
+    ){
+        Stock stock = stockRepository.findById(id).get();
+        model.addAttribute("stock", stock);
+        List<Product> products = productService.getProducts();
+        model.addAttribute("products",products);
+        return "stock-form";
     }
 }
