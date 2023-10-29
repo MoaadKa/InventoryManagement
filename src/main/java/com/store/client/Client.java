15f1f186edd68dd8634f794aa8a6dd6b11c8eb38
MoaadKa <moaadkarkouri@gmail.com>
2023-10-29 22:32:47 +0100

    made some changes

--- src/main/java/com/store/client/Client.java
+++ src/main/java/com/store/client/Client.java
@@ -7,14 +7,7 @@ import jakarta.persistence.*;
 @Table(name = "client")
 public class Client {
     @Id
-    @SequenceGenerator(
-            name = "client_sequence",
-            sequenceName = "client_sequence"
-    )
-    @GeneratedValue(
-            generator = "client_sequence",
-            strategy = GenerationType.SEQUENCE
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
