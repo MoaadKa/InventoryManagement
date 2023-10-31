844d5fa804ebd7621365f5c7b95a84cd09de49da
MoaadKa <moaadkarkouri@gmail.com>
2023-10-31 21:31:50 +0100

    Created a new method using JPQL Query to be able to search using a keyword

--- src/main/java/com/store/product/ProductRepository.java
+++ src/main/java/com/store/product/ProductRepository.java
@@ -1,8 +1,14 @@
 package com.store.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    @Query(" SELECT p FROM Product p WHERE p.name LIKE %?1%")
+    public List<Product> findAll(String keyword);
 }
